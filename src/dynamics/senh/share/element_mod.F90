8304c5887012cedb7cb3d96585bb4776669a51bf
Owen Hughes <okayhughes@gmail.com>
2025-04-30 17:09:25 -0600

    Adds preliminary support for the Deep-atmosphere version of senh

--- src/dynamics/senh/share/element_mod.F90
+++ src/dynamics/senh/share/element_mod.F90
@@ -130,6 +130,7 @@ module element_mod
      integer(kind=long_kind)  :: gdofP(np,np)                         ! global degree of freedom (P-grid)
 
      real (kind=real_kind)    :: fcor(np,np)                          ! Coriolis term
+     real (kind=real_kind)    :: fcorcosine(np,np)
 
      type (index_t) :: idxP
      integer :: FaceNum
